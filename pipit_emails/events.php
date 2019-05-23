77295b515a6e9c858fe7c8b4f5c1d727ff0e9c54
Hussein Al Hammad <husseinalhammad@gmail.com>
2019-05-23 17:28:08 +0400

    Disable event listener until app installed

--- pipit_emails/events.php
+++ pipit_emails/events.php
@@ -2,63 +2,70 @@
 
     $API  = new PerchAPI(1.0, 'pipit_emails');
     
-
-    if(!strpos($_SERVER['REQUEST_URI'], 'pipit_emails') !== false) {
-        $Emails = new PipitEmails_Emails($API);
-        $emails = $Emails->all();
+    $Settings = $API->get('Settings');
+    // only run if installed
+    if ($Settings->get('pipit_emails_update')->val()) {
+        if(!strpos($_SERVER['REQUEST_URI'], 'pipit_emails') !== false) {
+            $Emails = new PipitEmails_Emails($API);
+            $emails = $Emails->all();
+            
+            $API_Email = $API->get('Email');
+            $event_handlers = include(PerchUtil::file_path(PERCH_PATH . '/config/pipit_emails_event_handlers.php'));
+            
+            if(PerchUtil::count($emails)) {
+                foreach($emails as $Email) {
+                    if($Email->event()) {
+                        $API->on($Email->event(), function(PerchSystemEvent $Event) use($Email, $API_Email, $event_handlers){
+                            // event linked to an email was fired
+                            //PerchUtil::debug($Event);
+                            //PerchUtil::debug($Email);
+                            
         
-        $API_Email = $API->get('Email');
-        $event_handlers = include(PerchUtil::file_path(PERCH_PATH . '/config/pipit_emails_event_handlers.php'));
-
-        foreach($emails as $Email) {
-            if($Email->event()) {
-                $API->on($Email->event(), function(PerchSystemEvent $Event) use($Email, $API_Email, $event_handlers){
-                    // event linked to an email was fired
-                    //PerchUtil::debug($Event);
-                    //PerchUtil::debug($Email);
-                    
-
-                    $sender_name = PERCH_EMAIL_FROM_NAME;
-                    $sender_email = PERCH_EMAIL_FROM;
-                    if($Email->sender_name()) $sender_name = $Email->sender_name();
-                    if($Email->sender_email()) $sender_email = $Email->sender_email();
-
-                    $API_Email->senderName($sender_name);
-                    $API_Email->senderEmail($sender_email);
-                    $API_Email->replyToEmail($sender_email);
-                    $API_Email->subject($Email->subject());
-
-                    // get recepient email address(es)
-                    if($Email->from_event() == 1) {
-                        $recipient = '';
-                        foreach($event_handlers as $key => $handler) {
-                            if($key == $Event->event) {
-                                $recipient = $handler($Event);
-                                //PerchUtil::debug($recipient);
+                            $sender_name = PERCH_EMAIL_FROM_NAME;
+                            $sender_email = PERCH_EMAIL_FROM;
+                            if($Email->sender_name()) $sender_name = $Email->sender_name();
+                            if($Email->sender_email()) $sender_email = $Email->sender_email();
+        
+                            $API_Email->senderName($sender_name);
+                            $API_Email->senderEmail($sender_email);
+                            $API_Email->replyToEmail($sender_email);
+                            $API_Email->subject($Email->subject());
+        
+                            // get recepient email address(es)
+                            if($Email->from_event() == 1) {
+                                $recipient = '';
+                                foreach($event_handlers as $key => $handler) {
+                                    if($key == $Event->event) {
+                                        $recipient = $handler($Event);
+                                        //PerchUtil::debug($recipient);
+                                    }
+                                }
+        
+                                if(isset($recipient)) {
+                                    $API_Email->recipientEmail($recipient);
+                                }
+        
+                            } else {
+                                $API_Email->recipientEmail(explode(',', $Email->recipients()));
                             }
-                        }
-
-                        if(isset($recipient)) {
-                            $API_Email->recipientEmail($recipient);
-                        }
-
-                    } else {
-                        $API_Email->recipientEmail(explode(',', $Email->recipients()));
+        
+        
+                            if($Email->template() != '') {
+                                $API_Email->set_template('emails/'.$Email->template());
+                                $API_Email->template_method('perch');
+                                $API_Email->set_bulk($Email->to_array());
+                            }
+                            
+                            
+                            //PerchUtil::debug($API_Email);
+                            $API_Email->send();
+                        });
                     }
-
-
-                    if($Email->template() != '') {
-                        $API_Email->set_template('emails/'.$Email->template());
-                        $API_Email->template_method('perch');
-                        $API_Email->set_bulk($Email->to_array());
-                    }
-                    
-                    
-                    //PerchUtil::debug($API_Email);
-                    $API_Email->send();
-                });
+                }
             }
-        }
+        } 
+    } else {
+        PerchUtil::debug('Pipit Emails event listeners disabled. App installation incomplete.', 'notice');
     }
 
     
