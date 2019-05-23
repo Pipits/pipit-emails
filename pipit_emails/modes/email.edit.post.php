cd20d98e82e079596cedabc3e8112bb86da17df3
Hussein Al Hammad <husseinalhammad@gmail.com>
2019-05-23 18:50:47 +0400

    Add breadcrumbs to edit form

--- pipit_emails/modes/email.edit.post.php
+++ pipit_emails/modes/email.edit.post.php
@@ -7,7 +7,28 @@
     if($message) echo $message;
     
 
+    if (is_object($Email)) {
+        $Smartbar = new PerchSmartbar($CurrentUser, $HTML, $Lang);
 
+        $Smartbar->add_item([
+            'active' => true,
+            'type'  => 'breadcrumb',
+            'links' => [
+                [
+                    'title' => 'Emails',
+                    'link'  => $API->app_nav(),
+                    'translate' => false,
+                ],
+                [
+                    'title' => $Email->title(),
+                    'link'  => $API->app_nav() . '/edit/?id=' .$Email->id(),
+                    'translate' => false,
+                ]
+            ],
+        ]);
+
+        echo $Smartbar->render();
+    }
 
 
 
